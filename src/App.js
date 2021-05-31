1b7d9d3b495960307fc44d2bdcb31e8cecbe742b
santosh <santosh.ece06@gmail.com>
2021-05-31 16:53:36 +0530

    b2

--- src/App.js
+++ src/App.js
@@ -2,30 +2,5 @@ import logo from "./logo.svg";
 import "./App.css";
 
 export default function App() {
-  return (
-    <div>
-      <h1 style={{ background: "tomato", color: "white" }}>Project Title</h1>
-
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid vel
-        necessitatibus molestiae quis, recusandae quos tempora iste
-        reprehenderit, commodi enim expedita? Dolore molestias labore sed
-        eligendi, minus cum enim voluptatem. Lorem ipsum dolor sit amet
-        consectetur adipisicing elit. Quaerat explicabo alias deserunt et
-        quisquam esse voluptatem, amet sunt nihil perferendis! Eaque, illum?
-        Odio quidem voluptatum delectus! Repellendus eius eligendi sit.
-      </div>
-
-      <h2>Second Heading</h2>
-      <div>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, qui
-        facere vero et nihil est culpa, velit aspernatur natus adipisci nobis
-        earum. Doloremque voluptates excepturi adipisci sit sapiente ab
-        aspernatur. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-        Consequuntur vitae fuga nisi vel soluta. Laboriosam tempora, inventore
-        reiciendis provident magni dolor, magnam ab in veniam quam, optio
-        debitis aliquid quibusdam?
-      </div>
-    </div>
-  );
+  return <div>Hello World</div>;
 }
