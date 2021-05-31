7d23ff505ce1d361a7770ba1d1d8c4b4e8668746
santosh <santosh.ece06@gmail.com>
2021-05-31 16:52:50 +0530

    b1

--- src/App.js
+++ src/App.js
@@ -1,271 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { useState } from "react";
 
 export default function App() {
   return (
     <div>
-      <MyDynamicInputCompoent />
-      <MyInputCompoent />
+      <h1 style={{ background: "tomato", color: "white" }}>Project Title</h1>
+
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid vel
+        necessitatibus molestiae quis, recusandae quos tempora iste
+        reprehenderit, commodi enim expedita? Dolore molestias labore sed
+        eligendi, minus cum enim voluptatem. Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Quaerat explicabo alias deserunt et
+        quisquam esse voluptatem, amet sunt nihil perferendis! Eaque, illum?
+        Odio quidem voluptatum delectus! Repellendus eius eligendi sit.
+      </div>
+
+      <h2>Second Heading</h2>
+      <div>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, qui
+        facere vero et nihil est culpa, velit aspernatur natus adipisci nobis
+        earum. Doloremque voluptates excepturi adipisci sit sapiente ab
+        aspernatur. Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+        Consequuntur vitae fuga nisi vel soluta. Laboriosam tempora, inventore
+        reiciendis provident magni dolor, magnam ab in veniam quam, optio
+        debitis aliquid quibusdam?
+      </div>
     </div>
   );
 }
-
-const MyDynamicInputCompoent = () => {
-  const [list, setList] = useState([]);
-
-  // STEP 1
-  const [tweetInput, setTweetInput] = useState("");
-
-  // STEP 2 :: e is important here;
-  const readAndUpdateTweet = (e) => setTweetInput(e.target.value);
-
-  const postTheTweet = () => {
-    setList([tweetInput, ...list]);
-    setTweetInput("");
-  };
-
-  return (
-    <div>
-      <h2>Tweets</h2>
-      <div>
-        {/** STEP 3 value and onChange */}
-        <input
-          type="text"
-          value={tweetInput}
-          onChange={(e) => readAndUpdateTweet(e)}
-          placeholder="your thought..."
-        />
-        <input
-          type="button"
-          value="Tweet here"
-          onClick={() => postTheTweet()}
-        />
-      </div>
-
-      {list.map((item, index) => (
-        <div key={index}>{item}</div>
-      ))}
-    </div>
-  );
-};
-
-const MyInputCompoent = () => {
-  const [list, setList] = useState(["Delhi", "Kolkata"]);
-
-  const postTheTweet = () => {
-    setList(["MUMBAI", ...list]);
-  };
-
-  return (
-    <div>
-      <h1>Mini Twitter</h1>
-      <div>
-        <input type="text" value="" placeholder="your thought..." />
-        <input
-          type="button"
-          value="Tweet here"
-          onClick={() => postTheTweet()}
-        />
-      </div>
-
-      <h2>Tweets</h2>
-      {list.map((item, index) => (
-        <div key={index}>{item}</div>
-      ))}
-    </div>
-  );
-};
-
-const InlineStyleComponent = () => {
-  const [counter] = useState(100);
-
-  return (
-    <div>
-      <h1>Inline Styling</h1>
-      <div
-        style={{ backgroundColor: "royalblue", color: "white", padding: "8px" }}
-      >
-        {counter}
-      </div>
-    </div>
-  );
-};
-
-const ListExample2 = () => {
-  const [list, setList] = useState([1, 2]);
-
-  const addNewItem = () => {
-    // setList([...list, Math.random()]);
-    setList([Math.random(), ...list]);
-  };
-
-  return (
-    <div>
-      <h1>List Exampl2 </h1>
-      <div>
-        <input
-          type="button"
-          value="Add New Element"
-          onClick={(e) => addNewItem()}
-        />
-      </div>
-
-      {list.map((item, index) => (
-        <div key={index} className="beautify">
-          {item}
-        </div>
-      ))}
-    </div>
-  );
-};
-
-const ListExampleComponent = () => {
-  const [list, setList] = useState([1, Math.random()]);
-
-  const udpateList = () => {
-    // makeing a copy of existing list
-    const newList = [...list];
-
-    // addig new element
-    newList.unshift(Math.random());
-
-    // updating the list
-    setList(newList);
-
-    // setList([...list, Math.random()]);
-  };
-
-  return (
-    <div>
-      <h1>List Example Component</h1>
-      <div>
-        <input
-          type="button"
-          value="Add new Element"
-          onClick={() => udpateList()}
-        />
-      </div>
-
-      {list.map((item, index) => (
-        <div key={index}>{item}</div>
-      ))}
-    </div>
-  );
-};
-
-const ListComponent = () => {
-  const [counter] = useState(100);
-  const [username] = useState("mumbai");
-  const [list] = useState(["Delhi", "Calcutta", "Mumbai", "Mumbai", "Chennai"]);
-
-  return (
-    <div>
-      <h1>List Component</h1>
-      <h4>{counter}</h4>
-      <h4>{username}</h4>
-
-      {/** WE ARE LEARING HOW TO USE LIST */}
-      {list.map((item, index) => (
-        <h3 key={index}>{item}</h3>
-      ))}
-    </div>
-  );
-};
-
-// SUMMARY
-function FifthComponent() {
-  const [counter] = useState(100);
-  const [username] = useState("mumbai");
-
-  const updateUsername = () => {};
-  const updateCoutner = () => {};
-
-  return (
-    <div>
-      <h1>Fifth Component</h1>
-      <div>Counter {counter}</div>
-      <div>Username {username}</div>
-    </div>
-  );
-}
-
-// Equivalent to class in Java
-function FourthComponent() {
-  let localCounter = 100;
-  let [counter, setCounter] = useState(100);
-
-  const updateCounter = () => {
-    localCounter = localCounter + 1;
-
-    // counter = counter + 1;
-    setCounter(counter + 1);
-
-    console.log("Counter Operation", localCounter, counter);
-  };
-
-  // VIEW IS NOT GETTING UPDATED
-  return (
-    <div>
-      <h1>Fourth Component - Event / Action /Click </h1>
-      <input type="button" value="Update Counter" onClick={updateCounter} />
-      <input
-        type="button"
-        value="Update Counter1"
-        onClick={() => updateCounter()} // We can PASS PARAMETER here.
-      />
-
-      <div>React Not Aware - {localCounter} </div>
-      <div>React Aware - {counter}</div>
-    </div>
-  );
-}
-
-// Equivalent to class in Java
-function ThirdComponent() {
-  // NOT REACT AWARE
-  const localCounter = 100;
-
-  // REACT AWARE
-  const [counter] = useState(100);
-
-  // VIEW : JSX
-  return (
-    <div>
-      <h1>Third Component</h1>
-      <div>React Not Aware - {localCounter} </div>
-      <div>React Aware - {counter}</div>
-    </div>
-  );
-}
-
-/** SPECIAL FUNCTION :: USING REACT :: return jsx */
-// <h1> :: <MyComponent>
-const MyComponent = () => {
-  return (
-    <div>
-      <h1>React Heading </h1>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium
-        error aperiam temporibus sunt alias iusto ex facere blanditiis non
-        eligendi impedit a et rerum, cumque beatae harum fuga ut! Vero.
-      </div>
-
-      <h4>Heading 6</h4>
-      <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit assumenda
-        sequi, excepturi molestiae veniam, error officiis earum repellat iste
-        iure rerum sed sit placeat facere ex dignissimos! Temporibus, sit
-        facere.
-      </div>
-    </div>
-  );
-};
-
-const SecondComponent = () => {
-  return (
-    <div>
-      <h1>Second Component</h1>
-    </div>
-  );
-};
