28a9356cedf10f9d510c8bed0f7e18b16adfc969
santosh <santosh.ece06@gmail.com>
2021-05-31 12:27:33 +0530

    list.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,158 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 
 export default function App() {
   return (
     <div>
-      <MyComponent></MyComponent>
-      <SecondComponent></SecondComponent>
+      <ListExample2 />
+    </div>
+  );
+}
+
+const ListExample2 = () => {
+  const [list, setList] = useState([1, 2]);
+
+  const addNewItem = () => {
+    // setList([...list, Math.random()]);
+    setList([Math.random(), ...list]);
+  };
+
+  return (
+    <div>
+      <h1>List Exampl2 </h1>
+      <div>
+        <input
+          type="button"
+          value="Add New Element"
+          onClick={(e) => addNewItem()}
+        />
+      </div>
+
+      {list.map((item, index) => (
+        <div key={index} className="beautify">
+          {item}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+const ListExampleComponent = () => {
+  const [list, setList] = useState([1, Math.random()]);
+
+  const udpateList = () => {
+    // makeing a copy of existing list
+    const newList = [...list];
+
+    // addig new element
+    newList.unshift(Math.random());
+
+    // updating the list
+    setList(newList);
+
+    // setList([...list, Math.random()]);
+  };
+
+  return (
+    <div>
+      <h1>List Example Component</h1>
+      <div>
+        <input
+          type="button"
+          value="Add new Element"
+          onClick={() => udpateList()}
+        />
+      </div>
+
+      {list.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
+    </div>
+  );
+};
+
+const ListComponent = () => {
+  const [counter] = useState(100);
+  const [username] = useState("mumbai");
+  const [list] = useState(["Delhi", "Calcutta", "Mumbai", "Mumbai", "Chennai"]);
+
+  return (
+    <div>
+      <h1>List Component</h1>
+      <h4>{counter}</h4>
+      <h4>{username}</h4>
+
+      {/** WE ARE LEARING HOW TO USE LIST */}
+      {list.map((item, index) => (
+        <h3 key={index}>{item}</h3>
+      ))}
+    </div>
+  );
+};
+
+// SUMMARY
+function FifthComponent() {
+  const [counter] = useState(100);
+  const [username] = useState("mumbai");
+
+  const updateUsername = () => {};
+  const updateCoutner = () => {};
+
+  return (
+    <div>
+      <h1>Fifth Component</h1>
+      <div>Counter {counter}</div>
+      <div>Username {username}</div>
+    </div>
+  );
+}
+
+// Equivalent to class in Java
+function FourthComponent() {
+  let localCounter = 100;
+  let [counter, setCounter] = useState(100);
+
+  const updateCounter = () => {
+    localCounter = localCounter + 1;
+
+    // counter = counter + 1;
+    setCounter(counter + 1);
+
+    console.log("Counter Operation", localCounter, counter);
+  };
+
+  // VIEW IS NOT GETTING UPDATED
+  return (
+    <div>
+      <h1>Fourth Component - Event / Action /Click </h1>
+      <input type="button" value="Update Counter" onClick={updateCounter} />
+      <input
+        type="button"
+        value="Update Counter1"
+        onClick={() => updateCounter()} // We can PASS PARAMETER here.
+      />
+
+      <div>React Not Aware - {localCounter} </div>
+      <div>React Aware - {counter}</div>
+    </div>
+  );
+}
+
+// Equivalent to class in Java
+function ThirdComponent() {
+  // NOT REACT AWARE
+  const localCounter = 100;
+
+  // REACT AWARE
+  const [counter] = useState(100);
+
+  // VIEW : JSX
+  return (
+    <div>
+      <h1>Third Component</h1>
+      <div>React Not Aware - {localCounter} </div>
+      <div>React Aware - {counter}</div>
     </div>
   );
 }
